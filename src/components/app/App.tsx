ed3520c9fc0f359f033a2d7ae67354f6a76c3836
Feras Ahmad <feras.ahmad@ippen-digital.de>
2024-06-15 22:10:33 +0200

    config jest, vite, redux

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,5 +2,5 @@ import { ReactElement } from 'react';
 import { AppContainer } from 'components/app/styles';
 
 export default function App(): ReactElement {
-    return <AppContainer>App</AppContainer>;
+    return <AppContainer data-testid="app">App</AppContainer>;
 }
