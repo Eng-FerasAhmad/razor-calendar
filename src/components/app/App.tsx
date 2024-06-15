171779dc5c094c01cb301223c080628044321980
Feras Ahmad <feras.ahmad@ippen-digital.de>
2024-06-15 19:54:55 +0200

    config vite

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import {ReactElement} from "react";
+import {AppContainer} from "components/app/styles";
+
+export default function App():ReactElement {
+  return (
+    <AppContainer>
+        App
+    </AppContainer>
+  )
+}
