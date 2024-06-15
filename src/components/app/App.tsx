c2498bb3d300ed45bd301669e5b55ca71861af0e
Feras Ahmad <feras.ahmad@ippen-digital.de>
2024-06-15 22:28:00 +0200

    add usage of global style

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,12 @@
 import { ReactElement } from 'react';
 import { AppContainer } from 'components/app/styles';
+import { GlobalStyle } from 'style/global';
 
 export default function App(): ReactElement {
-    return <AppContainer data-testid="app">App</AppContainer>;
+    return (
+        <AppContainer data-testid="app">
+            <GlobalStyle />
+            <div>app</div>
+        </AppContainer>
+    );
 }
